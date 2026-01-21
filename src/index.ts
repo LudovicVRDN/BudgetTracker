92b80ee9bb80aa89a96ae3a8bbd8fb80fa21be6c
LudovicVRDN <vourdon@live.fr>
2026-01-21 13:27:52 +0100

    Add tsconfig

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/*Créer un système de CRUD permettant de gérer plus facilement le budget de l'utilisateur . Il aura accès à sa liste de dépenses ainsi qu'a sa liste de revenus qui devront être remplies via le formulaire proposé sur le bouton "Ajouter une nouvelle entrée".
+- Les données de ce formulaire seront stockés dans le localStorage afin de ne pas perdre les informations à chaque refresh de la page.
+- Les données stocker ainsi dans le LocalStorage seront ensuite ajouté dans le DOM sous forme de liste.
+- Les éléments des deux liste pourront être supprimées via un bouton.
